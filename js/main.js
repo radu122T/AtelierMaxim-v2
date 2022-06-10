20d66f99a3f9ab7a6f5c9f7be4a396515c5aa9e7
radu122T <radu.cudalb@yahoo.ro>
2022-06-10 17:05:48 +0300

    06.10.2022

--- js/main.js
+++ js/main.js
@@ -1,12 +1,104 @@
+let shop = document.getElementById("shop")
+
+let basket = JSON.parse(localStorage.getItem("data")) || []
+
+let generateShop = () => {
+  return (shop.innerHTML = shopItemsData
+    .map((x) => {
+      let { id, display, tip, price, desc, img } = x
+      let search = basket.find((x) => x.id === id) || []
+            return `
+    <div class="item ${display} ${tip}" id=product-id-${id}>
+      <div>
+          <img src=${img} alt="produs">
+            <div class="details">
+                <p>${desc}</p>
+                <div class="priceQuantity">
+                    <div class="buttons">
+                        <span>Cantitate:</span>
+                        <div class='quantityButtons'>
+                            <i onclick="decrement(${id})" class="fa-solid fa-minus"></i>
+                            <div id=${id} class="quantity">
+                                ${search.item === undefined ? 0 : search.item}
+                            </div>
+                            <i onclick="increment(${id})" class="fa-solid fa-plus"></i>
+                        </div>
+
+                    </div>
+                    <h2> ${price} Lei</h2>
+                </div>
+
+                <div class="productButtons">
+			    		    <button>Adauga in cos</button>
+			    		    <button><a href="/produs.html">Personalizeaza</a></button>
+			          </div>
+            </div>
+        </div>  
+      </div>
+    `
+    })
+    .join(""))
+}
+
+generateShop()
+
+let increment = (id) => {
+    let selectedItem = id
+    let search = basket.find((x) => x.id === selectedItem.id)
+  
+    if (search === undefined) {
+      basket.push({
+        id: selectedItem.id,
+        item: 1,
+      })
+    } else {
+      search.item += 1
+    }
+  
+    // console.log(basket)
+    update(selectedItem.id)
+    localStorage.setItem("data", JSON.stringify(basket))
+}
+
+
+  let decrement = (id) => {
+    let selectedItem = id
+    let search = basket.find((x) => x.id === selectedItem.id)
+  
+    if (search === undefined) return
+    else if (search.item === 0) return
+    else {
+      search.item -= 1
+    }
+    update(selectedItem.id)
+    basket = basket.filter((x) => x.item !== 0)
+    // console.log(basket)
+    localStorage.setItem("data", JSON.stringify(basket))
+  }
+  let update = (id) => {
+    let search = basket.find((x) => x.id === id)
+    // console.log(search.item)
+    document.getElementById(id).innerHTML = search.item
+    calculation()
+  }
+  
+  let calculation = () => {
+    let cartIcon = document.getElementById("cartAmount")
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0)
+  }
+  
+  calculation()
+
+
 //sortare
 
-const thirdSection =  document.getElementsByClassName('thirdSection')[0]
+const shp =  document.getElementsByClassName('shop')[0]
 const buttonsSecond = document.querySelectorAll('[data-second]')
 buttonsSecond.forEach(button => {
     button.addEventListener('click', () => {
         const offs = button.dataset.second
         if( offs === 'noutati') { 
-            Array.prototype.forEach.call(thirdSection.children, child => {
+            Array.prototype.forEach.call(shp.children, child => {
                 if (child.classList.contains('noutatiProduse')){
                     child.classList.remove('noDisplay') 
                     child.classList.add('display')
@@ -18,7 +110,7 @@ buttonsSecond.forEach(button => {
         })
     }
         else {
-            Array.prototype.forEach.call(thirdSection.children, child => {
+            Array.prototype.forEach.call(shp.children, child => {
                 if (child.classList.contains('celeMaiVanduteProduse')){
                     child.classList.remove('noDisplay') 
                     child.classList.add('display')}
@@ -31,25 +123,4 @@ buttonsSecond.forEach(button => {
 })
 
 
-// + - items on products
 
-const quantButton = document.querySelectorAll(".quantityButton")
-console.log(quantButton)
-quantButton.forEach(button => button.addEventListener("click", iter)) 
-function iter(evt) {
-    let oldValue = this.nextElementSibling
-    if (!oldValue) 
-        oldValue = this.previousElementSibling
-
-    if(evt.target.classList.contains('plus'))
-        if (oldValue.value>98)
-            oldValue.value===98
-        else{
-            oldValue.value++
-        }
-        
-    else if (oldValue.value>1) 
-        oldValue.value--
-    else
-        oldValue.value = 1
-}
